7d301148703167e81048c8dfc6d9e5a5755268fc
Rick Sellens <sellensr@gmail.com>
2020-01-05 13:06:24 -0500

    Rev 1 Hardware Changes
    Moved pins around to make connection wiring easier

--- Code/MP-Sim_AP/ch6teensyRev0.h
+++ Code/MP-Sim_AP/ch6teensyRev0.h
@@ -1,7 +1,6 @@
 #define NCH       4       // the number of actuator channels to use
 #define CH6TEENSY         // the 6 actuator version based on the Teensy 3.5
-                          // changes pin layout T3.5 pins 2~ through 8~ from Rev0 to better match connectors
-                          // reverses order on current sense pins A4 through A9
+                          // Rev 0 is the pin layout used in the first prototype electronics on the 4 channel hardware
 
 // include the SD library and set up to use:
 #include <SPI.h>
@@ -15,9 +14,9 @@ const int chipSelect = BUILTIN_SDCARD;  // for the Teensy 3.5
 //pins for each variable in array form, call motors 0 to 3 etc. with [0],[1],[2],[3] etc.
 
 // all down the left side leaving out pins 0,1 for Serial1 and 31,32 for Serial4
-unsigned char               _INA[] = { 2,  5,  8, 12, 25, 27}; // motor direction control pins A and B
-unsigned char               _INB[] = { 3,  6, 11, 24, 26, 28}; //      used to set CW, CCW, or Brake
-static const unsigned char  _PWM[] = { 4,  7,  9, 10, 29, 30}; // motor speed control pins
+unsigned char               _INA[] = { 3,  6,  2, 12, 25, 27}; // motor direction control pins A and B
+unsigned char               _INB[] = { 4,  7, 11, 24, 26, 28}; //      used to set CW, CCW, or Brake
+static const unsigned char  _PWM[] = { 5,  8,  9, 10, 29, 30}; // motor speed control pins
 
 // available on bottom. may not be needed. not used by Matt or up to 2019-01-18
 unsigned char           _ENGDIAG[] = {40, 41, 42, 43, 44, 45}; // motor driver diagnostic pins
@@ -27,43 +26,43 @@ const int              buttonPin[] = {39, 39, 39, 39, 39};     // pins for the a
 
 // all down the right side, leaving 39/A20 open for the big Red button
 // _CS not used by Matt or up to 2019-01-18
-unsigned char          _CS[] = { A9,  A8,  A7,  A6,  A5,  A4}; // motor current sense analog inputs
+unsigned char          _CS[] = { A4,  A5,  A6,  A7,  A8,  A9}; // motor current sense analog inputs
 unsigned char         _POS[] = {A21, A22,  A0,  A1,  A2,  A3}; // actuator position signals from pots 
 unsigned char         _FRC[] = {A14, A15, A16, A17, A18, A19}; // actuator force signals from load cells and amps
 /*  Hardware    Motor      Proto #  T3.5 Pins from Above
  *   Other      Drivers                     USB Port
- *   Sources    GND               1       GND        Vin
- *     AID                        2 RX1   0          Analog GND
- *                                3 TX1   1          3V3
- *              M0INA !           4       2~         A9   Current Sense M0 !
- *              M0INB !           5       3~         A8                 M1 !
- *              M0PWM !           6       4~         A7                 M2 !
- *              M1INA   !         7       5~         A6                 M3 !
- *              M1INB   !         8       6~         A5                 M4 !
- *              M1PWM   !         9       7~         A4                 M5 !
- *              M2INA !          10       8~         A3        Position M5    !
- *              M2PWM !          11       9~         A2                 M4    ! 
- *              M3PWM !          12      10~         A1                 M3    !
- *              M2INB !          13      11          A0                 M2    !
- *              M3INA !          14      12          13 LED                   !
- *              VDD   !          15      V3V         GND                      !
- *              M3INB !          16      24          A22       Position M1    !
- *              M4INA   !        17      25          A21                M0    !
- *              M4INB   !        18      26       39/A20    Big Red Button
- *              M5INA   !        19      27          A19       Force    M5    !
- *              M5INB   !        20      28          A18                M4    !
- *              M4PWM   !        21      29~         A17                M3    !
- *              M5PWM   !        22      30~         A16                M2    !
- *  (External                       RX4 A12          A15                M1    !
- *   Control)                       TX4 A13          A14                M0    !
+ *   Sources    GND              1       GND        Vin
+ *     AID                       2 RX1   0          Analog GND
+ *                               3 TX1   1          3V3
+ *              M2INA            4       2~         A9   Current Sense M5
+ *              M0INA            5       3~         A8                 M4
+ *              M0INB            6       4~         A7                 M3
+ *              M0PWM            7       5~         A6                 M2
+ *              M1INA            8       6~         A5                 M1
+ *              M1INB            9       7~         A4                 M0
+ *              M1PWM           10       8~         A3        Position M5
+ *              M2PWM           11       9~         A2                 M4 
+ *              M3PWM           12      10~         A1                 M3
+ *              M2INB           13      11          A0                 M2
+ *              M3INA           14      12          13 LED
+ *              VDD             15      V3V         GND
+ *              M3INB           16      24          A22       Position M1
+ *              M4INA           17      25          A21                M0
+ *              M4INB           18      26       39/A20    Big Red Button
+ *              M5INA           19      27          A19       Force    M5
+ *              M5INB           20      28          A18                M4
+ *              M4PWM           21      29~         A17                M3
+ *              M5PWM           22      30~         A16                M2
+ *  (External                      RX4 A12          A15                M1
+ *   Control)                      TX4 A13          A14                M0
  *                                         SD Card
  *                                         
- *              M5EN            45
- *              M4EN            44
- *              M3EN            43
- *              M2EN            42
- *              M1EN            41
- *              M0EN            40
+ *              M5EN           45
+ *              M4EN           44
+ *              M3EN           43
+ *              M2EN           42
+ *              M1EN           41
+ *              M0EN           40
 *                                         
  *                                         
  *                                         
